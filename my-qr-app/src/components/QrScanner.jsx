98f2af67b2dd6caaed4ebf03b68775dd50e094b5
ShubhamTiwari55 <stofficial059@gmail.com>
2023-11-15 21:46:48 +0530

    fixed issue related to import statement

--- my-qr-app/src/components/QrScanner.jsx
+++ my-qr-app/src/components/QrScanner.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import QrReader from 'react-qr-reader';
+import { QrReader } from 'react-qr-reader';
 
 const QRScanner = () => {
   const [result, setResult] = useState('');
@@ -11,18 +11,25 @@ const QRScanner = () => {
   };
 
   const handleError = (error) => {
-    console.error(error);
+    console.error('Error while scanning QR code:', error);
+    // Add additional handling if needed
   };
 
   return (
     <div>
+      {/* Log information to console for debugging */}
+      <p>Current result: {result}</p>
+      
+      {/* Render the QR code scanner */}
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
         style={{ width: '100%' }}
       />
-      <p>{result}</p>
+
+      {/* Display the scanned result */}
+      <p>Scanned result: {result}</p>
     </div>
   );
 };
