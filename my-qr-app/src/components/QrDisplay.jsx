e959bbd8a67106de5df4e8fc6b49eccd40de6afc
Mishragini <mishragini11.com>
2023-11-22 17:32:08 +0530

    add cdp to the qr code

--- my-qr-app/src/components/QrDisplay.jsx
+++ my-qr-app/src/components/QrDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import QRCode from 'qrcode.react';
 
 const QRCodeDisplay = ({ itemId }) => {
   const [qrCode, setQRCode] = useState('');
@@ -17,7 +18,7 @@ const QRCodeDisplay = ({ itemId }) => {
     generateQRCode();
   }, [itemId]);
 
-  return <img src={qrCode} alt={`QR Code for Item ${itemId}`} />;
+  return <QRCode value={qrCode} />;
 };
 
 export default QRCodeDisplay;
